c0a4765fd1f97574f643d88850eefe2125395f69
Adigbonu Mmaduabuchi Franklin <adigbonumaduabuchi@gmail.com>
2025-11-01 04:14:42 +0100

    created project with create react app and added files for hompage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import react from "react";
+import './App.css';
+import HomePage from "./homepage.component";
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;

155975d04ff1827345c7b5654dddb326b0a468ae
Adigbonu Mmaduabuchi Franklin <adigbonumaduabuchi@gmail.com>
2025-11-04 16:03:35 +0100

    broken our homepage into smaller components,finish styling of menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+
+import Directory from "../../components/directory/direectory.component";
+
+import './homepage.styles.css';
+
+const HomePage  = () => (
+    <div className="homepage">
+        <Directory/>
+        </div>   
+);
+
+export default HomePage;
+    

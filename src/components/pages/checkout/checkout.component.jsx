8ba1edad6cc10e786bd4bee0fc257d96898dd722
Adigbonu Mmaduabuchi Franklin <adigbonumaduabuchi@gmail.com>
2026-04-08 19:02:18 +0100

    updating stripe integration and updating our favicon and title

--- src/components/pages/checkout/checkout.component.jsx
+++ src/components/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 
 import { selectCartItems, selectcartTotal } from "../../../redux/cart/cart.selectors";
 import CheckoutItem from './../checkout-item/checkout-item.component';
+import StripeCheckoutButton from "../../stripe-botton/stripe-button.component";
 import './checkout.styles.css';
 
 const CheckoutPage = () => {
@@ -34,10 +35,17 @@ return (
         <CheckoutItem key={ cartItem.id}cartItem={cartItem}/>
        ))}; 
 
-      <div className="total">
-       <span>TOTAL: ${total}</span> 
+      <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br/>
+      4242 4242 4242 4242 -Exp: 01/27 - CW:123
+    </div >
+        <div className="button">
+          <StripeCheckoutButton price={total}/>
         </div>
-    </div>
+        </div>
+    
 )};
 
 
---
+++ src/components/stripe-botton/stripe-button.component.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price})=>{
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_51THPFL0cjOWG18FUg4pius0dBiEUdYTxeHOOlrKDzmyheq4HpQebFRTnTht2ccGzMFiHsgCuVGPI8BH1PfTPfCi100RI0PBx1q"
+
+
+  const onToken = token =>{
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+ return (
+  <StripeCheckout
+  label='Pay Now'
+  name='Ozioma clothing Ltd.'
+  billingAddress
+  shippingAddress
+  image='//https://svgshare.com/i/CUz.svg'
+  description={`Your total is $${price}`}
+  amount ={priceForStripe}
+  panelLabel='Pay Now'
+  token ={onToken}
+  stripeKey={publishableKey}
+  />
+ );
+};
+
+export default StripeCheckoutButton;

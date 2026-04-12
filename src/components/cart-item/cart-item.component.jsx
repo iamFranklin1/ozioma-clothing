0225a84127c777e61affb416f163c19755e78715
Adigbonu Mmaduabuchi Franklin <adigbonumaduabuchi@gmail.com>
2026-04-12 18:58:01 +0100

    updating the multiplication sign on the cartItem component from raw character to HTML entity

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -9,7 +9,7 @@ const CartItem =({item: {imageUrl, price, name,quantity,} })=>(
         <div className="item-details">
         <span className="name">{name} </span>
         <span className="price">
-            {quantity} × ${price} </span> 
+            {quantity}&times;${price} </span> 
         </div>
     </div>
 );

8b451c31c739808f6b75c40379924d27370f50fa
Adigbonu Mmaduabuchi Franklin <adigbonumaduabuchi@gmail.com>
2026-04-15 15:28:13 +0100

    romiving redux logger  from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -18,9 +18,16 @@ import rootReducer from './root-reducer'
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(logger),
-  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) =>{
+    const middleswares = getDefaultMiddleware();
+  
+  if (process.env.NODE_ENV ==='development') {
+    middleswares.concat(logger);
+    return middleswares.concat(logger);
+  }
+  return middleswares;
+},
+ devTools: process.env.NODE_ENV !== 'production',
 });
 
 export const persistor = persistStore(store);
